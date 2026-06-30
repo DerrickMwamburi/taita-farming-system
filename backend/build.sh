e3d1aeecf6961890d20bb0a120c66b24e0520243
DerrickMwamburi <mwamburi60@gmail.com>
2026-06-30 09:02:21 +0300

    fix: auto-migrate and create superuser on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# 1. This will fix the 500 error by creating the PostgreSQL tables
 python manage.py migrate
+
+# 2. This creates your cloud Admin account. 
+# The "|| true" part tells the script not to crash if the user already exists on future deploys.
+python manage.py createsuperuser --noinput --username katute --email katute80@gmail.com || true
